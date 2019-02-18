de106c6a8597cad8051297356a0928f976c78892
Mia Erbus <mia.erbus@gmail.com>
2019-02-18 20:56:26 +0100

    User inputs a task and add it to task list on click

--- js/todo.js
+++ js/todo.js
@@ -1,31 +1,15 @@
-// get new task from user and write it to a variable (prompt?)
-let tasks = [];
-let currentTask;
-let isFinished = false;
-let answer;
+function addTaskToList() {
+  let currentTask = document.getElementById('text').value;
 
-do {
-  //if empty, try again until some valid value (loop?)
-  currentTask = prompt('Enter task:');
-
-  // if value is "nothing", notify user ("You can do better then that")
-  // (you can use alert)
-  if (currentTask == '') {
-    alert('You can do better then that');
+  if (currentTask == 'undefined' || currentTask == '') {
+    return false;
   }
-  // if value is anything else
-  // ask user if task is done, and request YES/NO,
-  // then write it to another variable as boolean
-  else {
-    var node = document.createElement("li");                 // Create a <li> node
-    var textnode = document.createTextNode(currentTask);     // Create a text node
-    node.appendChild(textnode);                              // Append the text to <li>
-    document.getElementById("myTasks").appendChild(node);    // Append <li> to <ul> with id="myTasks"
 
-    tasks.push(currentTask);
-    answer = prompt('Are you done? YES/NO');
-    if (answer === 'YES') {
-      isFinished = true;
-    }
-  }
-} while (tasks.length == 0 || isFinished == false)
+  let node = document.createElement('li');              // Create a <li> node
+  let textnode = document.createTextNode(currentTask);  // Create a text node
+  node.appendChild(textnode);                           // Append the text to <li>
+  document.getElementById('list').appendChild(node);    // Append <li> to <ul>
+  document.getElementById('text').value = '';
+};
+
+document.getElementById('button').addEventListener('click', addTaskToList);
