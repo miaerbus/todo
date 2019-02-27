462d6076168edc065cc912b53122c8da51ee7740
Mia Erbus <mia.erbus@gmail.com>
2019-02-27 17:45:59 +0100

    Add Task and TaskList objects

--- js/todo.js
+++ js/todo.js
@@ -1,15 +1,68 @@
-function addTaskToList() {
-  let currentTask = document.getElementById('text').value;
 
-  if (currentTask == 'undefined' || currentTask == '') {
-    return false;
-  }
+let list = document.getElementById('list');
+let myList = new TaskList();
+
+function addTaskToList() {
+  let currentTask = new Task();
+
+  currentTask.setTaskName(document.getElementById('text').value);
+  myList.add(currentTask);
 
   let node = document.createElement('li');              // Create a <li> node
-  let textnode = document.createTextNode(currentTask);  // Create a text node
+  let textnode = document.createTextNode(currentTask.getTaskName());  // Create a text node
   node.appendChild(textnode);                           // Append the text to <li>
-  document.getElementById('list').appendChild(node);    // Append <li> to <ul>
+  list.appendChild(node);                               // Append <li> to <ul>
   document.getElementById('text').value = '';
-};
+  document.getElementById('count').value = myList.countList();
+}
 
 document.getElementById('button').addEventListener('click', addTaskToList);
+
+
+function Task() {
+  let task = null;
+  let isFinished = null;
+  let dateCreated = null;
+
+  this.getTaskName = function() {
+    return task;
+  }
+  this.setTaskName = function(taskName) {
+    task = taskName;
+  }
+  this.getIsFinished = function() {
+    return isFinished;
+  }
+  this.getDatum = function() {
+    return dateCreated;
+  }
+  this.setDatum = function(date) {
+    dateCreated = date; // date.format ne dela
+  }
+  this.markFinished = function() {
+    isFinished = true;
+  }
+  this.markNotFinished = function() {
+    isFinished = false;
+  }
+};
+
+function TaskList() {
+  let list = [];
+  let count = 0;
+
+  this.getTaskList = function() {
+    return list;
+  }
+  this.setTaskList = function(listName) {
+    list = listName;
+  }
+  this.add = function(task) {
+    list.push(task);
+    count++;
+  }
+  this.countList = function() {
+    console.log(list.length);
+    return list.length;
+  }
+};
